efc88612a4d0a890a4e9b6c003dd34c2f83c9727
FelippeRibeiro <felipper43@gmail.com>
2023-06-24 16:45:18 -0300

    feat: :ambulance: Show error to user

--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -2,8 +2,7 @@
 import React from "react";
 import Card from "../card/Card";
 import { useQuery } from "react-query";
-import { api } from "@/api-service";
-import { IGames } from "@/api-service";
+import { IGames, api } from "@/api-service";
 import { useState } from "react";
 import { AxiosError } from "axios";
 
@@ -22,50 +21,60 @@ export default function Games() {
     onError: (error: AxiosError) => error,
   });
 
-  let filteredGames =
-    filter.length > 0
-      ? data?.games?.filter((game) => game.title.toLowerCase().includes(filter.toLowerCase()))
-      : data?.games;
+  let filteredGames = filter.length
+    ? data?.games?.filter((game) => game.title.toLowerCase().includes(filter.toLowerCase()))
+    : data?.games;
 
   if (genre.length) filteredGames = filteredGames?.filter((game) => game.genre === genre);
 
-  // if (isLoading)
-  //   return (
-  //     <div className="flex justify-center items-center h-screen">
-  //       <div className="custom-loader"></div>
-  //     </div>
-  //   );
-  // else
-  return (
-    <div className="flex justify-center flex-col">
-      <section className="flex justify-center">
-        <input
-          type="text"
-          className="p-1 "
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          placeholder="Busque um jogo..."
-        />
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="custom-loader"></div>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-red-600">
+          {error.message}
+          😭
+        </h1>
+      </div>
+    );
+  else
+    return (
+      <div className="flex justify-center flex-col">
+        <section className="flex justify-center">
+          <input
+            type="text"
+            className="p-1 "
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            placeholder="Busque um jogo..."
+          />
 
-        <select name="" id="" onChange={(e) => setGenres(e.target.value)}>
-          <option value={""}>Selecione um genero</option>
-          {data?.genres?.sort().map((game) => (
-            <option key={game} value={game}>
-              {game}
-            </option>
-          ))}
-        </select>
-      </section>
+          <select name="" id="" onChange={(e) => setGenres(e.target.value)}>
+            <option value={""}>Selecione um genero</option>
+            {data?.genres?.map((game) => (
+              <option key={game} value={game}>
+                {game}
+              </option>
+            ))}
+          </select>
+        </section>
 
-      <div className="flex flex-col justify-center items-center">
-        <div className="grid grid-cols-3 max-lg:grid-cols-1 max-xl:grid-cols-2 p-9 gap-4">
-          {filteredGames?.map((game) => (
-            <Card key={game.id} {...game}></Card>
-          ))}
+        <div className="flex flex-col justify-center items-center">
+          <div className="grid grid-cols-3 max-lg:grid-cols-1 max-xl:grid-cols-2 p-9 gap-4">
+            {filteredGames?.length ? (
+              filteredGames?.map((game) => <Card key={game.id} {...game}></Card>)
+            ) : (
+              <div className="text-center text-white">Nenhum resultado encontrado</div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 async function getGames() {
@@ -80,7 +89,7 @@ async function getGames() {
       if (!genres.includes(game.genre)) genres.push(game.genre);
     });
 
-    return { games, genres };
+    return { games, genres: genres.sort() };
   } catch (error) {
     console.log(error);
     throw error;
